e283fd5bf57e9cd1aac7c7820a2a333756286379
Steven Leimberg <smleimberg@gmail.com>
2014-05-20 09:47:09 -0500

    better dividiers

--- fer-settings-sample.php
+++ fer-settings-sample.php
@@ -1,7 +1,7 @@
 <?php
 $bitbucket_email = 'name@email.com';
 $folder_list=array(
-'Project Name 1'=>'~/www/project_1',
-'A Second Project'=>'/var/www/project_2'
+	'Project Name 1'=>'~/path_to_project_1',
+	'A Second Project'=>'/var/www/project_2'
 );
 ?>
--- fer.php
+++ fer.php
@@ -7,18 +7,16 @@ $date = '';
 // -s git --since option. Ex: "3.days" "2.weeks" "2.months"
 if(!empty($opts['s'])){
 	$since = ' --since='.$opts['s'];
-	echo $since."\n";
 }
 
 // -d egrep the results. you can use it to filter for specific days. EX: "2013-11-22|2013-11-25" for commits on friday & monday
 if(!empty($opts['d'])){
 	$date = ' | egrep "'.$opts['d'].'"';
-	echo $date."\n";
 }
 
 $output_array = array();
 foreach($folder_list as $project => $folder){
-	$command = 'git log --pretty=format:"%ad :'.$project.': %s"'.$since.' --committer="'.$bitbucket_email.'" --date=iso'.$date;
+	$command = 'git log --pretty=format:"%ai %Cgreen::'.$project.'::%Creset %Cred%s%Creset"'.$since.' --committer="'.$bitbucket_email.'" --all --date=iso'.$date;
 	$tmp = shell_exec('cd '.$folder.' && '.$command);
 	foreach(preg_split("/((\r?\n)|(\r\n?))/", $tmp) as $line){
 		if(!empty($line) && $line!='' && $line){
@@ -31,10 +29,11 @@ $last_date='';
 foreach($output_array as $commit){
 	$current_date = substr($commit, 0, 10);
 	if($last_date != $current_date){
-		echo "\n";
+		echo "--------------------------------------------------------------------------------\n";
 	}
 	echo $commit."\n";
 	$last_date = substr($commit, 0, 10);
 }
+echo "--------------------------------------------------------------------------------\n";
 
 ?>
