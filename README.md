a5a5b61c2cdec7e528614919d3ecb910cb1769ea
Steven Leimberg <smleimberg@gmail.com>
2014-06-17 09:43:13 -0500

    rename & add readme

---
+++ README.md
@@ -0,0 +1,3 @@
+#Commits By Day
+
+View the commit history of multiple repos ordered by date and time.
---
+++ cbd-settings.php
@@ -0,0 +1,14 @@
+<?php
+$bitbucket_email = 'smleimberg@gmail.com';
+$folder_list=array(
+	'AR Fence'=>'~/matmon/sites/arfence',
+	'Matmon'=>'~/matmon/sites/matmon_com',
+	'Privacystar'=>'~/matmon/sites/privacystar',
+	'Querencia'=>'~/matmon/sites/loscabosquerencia',
+	'Ritter Communications'=>'~/matmon/sites/ritter',
+	'Smile Dailey'=>'~/matmon/sites/smiledailey',
+	'SSN'=>'~/matmon/sites/ssn',
+	'Summit Bank'=>'~/matmon/sites/summit',
+	'Westrock'=>'~/matmon/sites/westrockcoffee'
+);
+?>
